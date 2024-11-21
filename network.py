e1712bc664eae57f613d831c46997b87a51db936
Antoine Beaugas <abeaugas@gmail.com>
2024-11-21 11:47:08 +0100

    added comments

--- network.py
+++ network.py
@@ -43,7 +43,7 @@ class CNNNetwork(nn.Module):
         self.conv1=nn.Sequential(
             nn.Conv2d(in_channels=1,out_channels=16,kernel_size=3,stride=1,padding=2),
             nn.ReLU(), # Activation function
-            nn.MaxPool2d(kernel_size=2)
+            nn.MaxPool2d(kernel_size=2) # Keep max value in kernel
         )
         self.conv2=nn.Sequential(
             nn.Conv2d(in_channels=16,out_channels=32,kernel_size=3,stride=1,padding=2),
